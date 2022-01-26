f7630a0eac31603372431156c33b7581f11fad30
Chris Kim <ckim@knockcrm.com>
2022-01-25 22:43:59 -0800

    added comments

--- graphene_workshop/schema/query.py
+++ graphene_workshop/schema/query.py
@@ -4,6 +4,10 @@ import graphene
 class Query(graphene.ObjectType):
     hello = graphene.String()
 
+    # Add something here
+
     @classmethod
     def resolve_hello(cls, root, info):
         return "Welcome!"
+
+    # Create a resolver for something here
